8329c9cbee2a44e413b5191bb08bbdf57af15840
nicolethoen <nthoen@redhat.com>
2022-11-23 14:21:35 -0500

    feat: fix npm issue?

--- release.config.js
+++ release.config.js
@@ -8,9 +8,8 @@ module.exports = {
   },
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    ['@semantic-release/npm', { pkgRoot: 'dist' }]
+    '@semantic-release/npm'
   ],
   tagFormat: 'prerelease-v${version}'
 };
