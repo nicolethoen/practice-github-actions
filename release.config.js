7563c8c3a15852e544b0dd8b0f21379d98a55602
nicolethoen <nthoen@redhat.com>
2022-12-19 13:57:16 -0500

    feat: release notes generator

--- release.config.js
+++ release.config.js
@@ -9,7 +9,8 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/github',
-    '@semantic-release/npm'
+    '@semantic-release/npm',
+    '@semantic-release/release-notes-generator'
   ],
   tagFormat: 'prerelease-v${version}'
 };
