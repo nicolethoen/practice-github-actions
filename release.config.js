9181ed6669688dd8ec241defbb740a8b04ed2889
Dana Gutride <dgutride@gmail.com>
2022-12-19 15:09:18 -0500

    fix(plugins): Modify plugins to use correct order

--- release.config.js
+++ release.config.js
@@ -9,8 +9,9 @@ module.exports = {
   },
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/github',
-    '@semantic-release/npm'
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
   ],
   tagFormat: 'prerelease-v${version}'
 };
