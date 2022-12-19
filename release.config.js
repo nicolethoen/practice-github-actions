84d7b10e31fad492595ff8cddf194eaa34780189
Dana Gutride <dgutride@gmail.com>
2022-12-19 14:55:39 -0500

    fix(release): Update release again

--- release.config.js
+++ release.config.js
@@ -1,17 +1,16 @@
 module.exports = {
   branches: [
-    'main', 
+    'main',
     { name: 'main', channel: 'prerelease' },
-    { name: 'v2', channel: 'alpha', prerelease: true}
+    { name: 'v2', range: '2.0.x', channel: 'alpha', prerelease: true}
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    '@semantic-release/npm'
   ],
   tagFormat: 'prerelease-v${version}'
 };
