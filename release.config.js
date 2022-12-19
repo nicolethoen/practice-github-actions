1e65636afb10647357e6001fac0d58521e92c618
Dana Gutride <dgutride@gmail.com>
2022-12-19 14:51:21 -0500

    fix(update): New release with prerelease true

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: [
     'main', 
     { name: 'main', channel: 'prerelease' },
-    { name: 'v2', channel: 'alpha', prerelease: 'alpha'}
+    { name: 'v2', channel: 'alpha', prerelease: true}
   ],
   analyzeCommits: {
     preset: 'angular'
