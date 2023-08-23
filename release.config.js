056efb0000f5df3f1781cd1a7b3f60fd73c14d9c
nicolethoen <nthoen@redhat.com>
2023-08-23 14:01:41 -0400

    fix: try to force everything to be a patch on this branch

--- release.config.js
+++ release.config.js
@@ -3,13 +3,22 @@ module.exports = {
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: '5.4.1', channel: 'prerelease-bugfix', range: '5.4.x' }
+    { name: '5.4.1', channel: 'prerelease-bugfix' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          { type: "feat", release: "patch" },
+          { type: "fix", release: "patch" }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github'
