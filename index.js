f21b9b8d3cc35fcace2585fbaf038988f2d7c0b6
Nicole Thoen <nthoen@redhat.com>
2023-08-30 09:14:45 -0400

    fix: see if do-not-delete tag helps

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export function breakingChanges() {
-  console.log("this has future patch on main branch. current patch on 5.4.x branch");
+  console.log("this has future prerelease on do-not-delete branch. current patch on 5.4.x branch");
 }
